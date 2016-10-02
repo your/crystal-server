13845d30146cac1a7f117df50b14f9cd1241e2a2
Giuseppe Lobraico <your@users.noreply.github.com>
2016-10-02 02:19:47 +0100

    Initial commit

---
+++ store.cr
@@ -0,0 +1,25 @@
+class Store
+  def initialize(@base_dir = "./store")
+    @resources = {} of String => String
+  end
+
+  def resource(path)
+    filename = @base_dir + path + "." + extension(path)
+
+    if @resources[path]? # Lazy read =)
+      puts "[cache] Serving #{filename}"
+    else
+      puts "Serving #{filename}"
+      @resources[path] = File.read(filename)
+    end
+
+    @resources[path]
+  rescue
+    puts "Error: #{filename} could not be served"
+    nil
+  end
+
+  def extension(path)
+    path.split("/")[-2]
+  end
+end
